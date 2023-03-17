01d2b04eecc6fc0b79b50b7a6be741c55eed1f92
mhpillonetto <marcelohenriquepillonetto@gmail.com>
2023-03-17 15:40:34 -0300

    add: ship detail from service

--- src/pages/Shipment/detail.tsx
+++ src/pages/Shipment/detail.tsx
@@ -5,16 +5,13 @@ const ShipmentDetail = () => {
     let { state } = useLocation();
     const shipment = state;
 
-    console.log('====================================');
-    console.log(new Date(shipment.createdAt).toLocaleTimeString());
-    console.log('====================================');
-
     return (
         <div className='container mt-5'>
             <h1>{shipment.name}</h1>
             <div className='mt-3'>
                 <h3>De {state.retrievalLocation} para {shipment.deliveryLocation}</h3>
                 <h4>Postado por {shipment.owner}</h4>
+                <h4>Veículo requisitado: {shipment.requiredVehicle}</h4>
                 <h4>Criado as {new Date(shipment.createdAt).toLocaleTimeString()} de {new Date(shipment.createdAt).toLocaleDateString()}</h4>
             </div>
 
